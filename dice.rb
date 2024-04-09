557001766f3d697cc4712b828c497f593ba2b55d
mattmartinez45 <114887556+mattmartinez45@users.noreply.github.com>
2024-04-09 18:28:52 +0000

    more edits

--- dice.rb
+++ dice.rb
@@ -41,11 +41,11 @@ get("/dice/2/10") do
 end
 
 get("/dice/1/20") do
-  die = rand(1..20)
-	
-	@outcome = "You rolled a #{die}."
-	
-	erb(:one_twenty)
+  @die = rand(1..20)
+
+  @outcome = "You rolled a #{@die}."
+
+  erb(:one_twenty)
 end
 
 get("/dice/5/4") do
@@ -58,4 +58,16 @@ get("/dice/5/4") do
 	@outcome = "You rolled a #{first_die}, a #{second_die}, a #{third_die}, and a #{fourth_die} for a total of #{sum}."
 	
   erb(:five_four)
+end
+
+get("/dice/100/6") do
+  @rolls = []
+
+  100.times do
+    die = rand(1..6)
+
+    @rolls.push(die)
+  end
+
+  erb(:one_hundred_six)
 end
